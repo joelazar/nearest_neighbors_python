7ab2746049e6e216423781e38c888708920e6f00
joelazar <jozsef.gabor.lazar@gmail.com>
2017-07-30 20:25:20 +0200

    logSolution method introduced, print time only when main called

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -9,6 +9,7 @@ class InputData:
     def __init__(self, inputfile):
         try:
             self.array = numpy.loadtxt(inputfile)
+            self.solution = None
             #print self.array
         except IOError:
             print "Wrong path to testdata given"
@@ -17,28 +18,25 @@ class InputData:
     def findNearestNeighbors(self):
         nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(self.array)
         (distances, indices) = nbrs.kneighbors(self.array)
-        return indices[numpy.argmin(numpy.delete(distances,0,1))]
+        self.solution = indices[numpy.argmin(numpy.delete(distances,0,1))]
 
+    def logSolution(self):
+        print "%d:%s" % (self.solution[0]+1,'\t'.join(map(str,[int(round(floater)) for floater in self.array[self.solution[0]]])))
+        print "%d:%s" % (self.solution[1]+1,'\t'.join(map(str,[int(round(floater)) for floater in self.array[self.solution[1]]])))
 
 def nearest_neighbors(inputfile):
 
-    start = time.time()
-
     data = InputData(inputfile)
-
-    solution = data.findNearestNeighbors()
-
-    print "%d:%s" % (solution[0]+1,'\t'.join(map(str,[int(round(floater)) for floater in data.array[solution[0]]])))
-    print "%d:%s" % (solution[1]+1,'\t'.join(map(str,[int(round(floater)) for floater in data.array[solution[1]]])))
-
-    end = time.time()
-    elapsed = end - start
-    #print "Elapsed time : %.3f seconds" % elapsed
-
+    data.findNearestNeighbors()
+    data.logSolution()
 
 if __name__ == '__main__':
     try:
+        start = time.time()
         nearest_neighbors(sys.argv[1])
+        end = time.time()
+        elapsed = end - start
+        print "Elapsed time : %.3f seconds" % elapsed
     except IndexError:
         print "No param was given to the script - example usage: " \
             "./nearest_neighbors.py PATH_TO_TESTDATA"
