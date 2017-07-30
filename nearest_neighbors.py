204223d26aecb7cfb3e72ddfc5e0e0c84ae191c0
joelazar <jozsef.gabor.lazar@gmail.com>
2017-07-30 15:49:28 +0200

    InputData class introduced, main added

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -4,30 +4,42 @@ import time
 import numpy
 from sklearn.neighbors import NearestNeighbors
 
-start = time.time()
 
-try:
-    param = sys.argv[1]
-    my_array = numpy.loadtxt(param)
-except IndexError:
-    print "No param was given to the script - example usage: " \
-          "./nearest_neighbors.py PATH_TO_TESTDATA"
-    sys.exit(1)
-except IOError:
-    print "Wrong path to testdata given"
-    sys.exit(1)
+class InputData:
+    def __init__(self, inputfile):
+        try:
+            self.array = numpy.loadtxt(inputfile)
+            #print self.array
+        except IOError:
+            print "Wrong path to testdata given"
+            sys.exit(1)
 
-print my_array
+    def findNearestNeighbors(self):
+        nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(self.array)
+        (distances, indices) = nbrs.kneighbors(self.array)
+        return indices[numpy.argmin(numpy.delete(distances,0,1))]
 
-nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(my_array)
-distances, indices = nbrs.kneighbors(my_array)
 
-solution = indices[numpy.argmin(numpy.delete(distances,0,1))]
+def nearest_neighbors(inputfile):
 
-print "%d:%s" % (solution[0]+1,' '.join(map(str,[int(floater) for floater in my_array[solution[0]]])))
-print "%d:%s" % (solution[1]+1,' '.join(map(str,[int(floater) for floater in my_array[solution[1]]])))
+    start = time.time()
 
-end = time.time()
-elapsed = end - start
+    data = InputData(inputfile)
 
-print "Elapsed time : %.3f seconds" % elapsed
+    solution = data.findNearestNeighbors()
+
+    print "%d:%s" % (solution[0]+1,'\t'.join(map(str,[int(round(floater)) for floater in data.array[solution[0]]])))
+    print "%d:%s" % (solution[1]+1,'\t'.join(map(str,[int(round(floater)) for floater in data.array[solution[1]]])))
+
+    end = time.time()
+    elapsed = end - start
+    #print "Elapsed time : %.3f seconds" % elapsed
+
+
+if __name__ == '__main__':
+    try:
+        nearest_neighbors(sys.argv[1])
+    except IndexError:
+        print "No param was given to the script - example usage: " \
+            "./nearest_neighbors.py PATH_TO_TESTDATA"
+        sys.exit(1)
